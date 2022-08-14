10b5538835974728c84e9cf86665b2eeee2a3b08
mvchn <movchan@gmail.com>
2022-08-14 11:15:35 +0300

    100% coverted, assert fails

--- src/main/java/store/Store.java
+++ src/main/java/store/Store.java
@@ -1,8 +1,29 @@
 package store;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Store {
 
+    private final List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    public Product findByName(String name){
+        //TODO: check demetra law
+        return products.stream().filter(product -> product.getName().equals(name)).findFirst().orElseThrow(() -> new IllegalArgumentException("There is no such product -> " + name));
+    }
+
     public double calculateTotalCost(String query) {
-        return 13.25;
+        char[] index = query.trim().toCharArray();
+        double result = 0.0;
+        for (char c : index) {
+            Product product = findByName(String.valueOf(c));
+            result += product.getPrice();
+        }
+
+        return result;
     }
 }
--- src/test/java/store/StoreTest.java
+++ src/test/java/store/StoreTest.java
@@ -8,7 +8,12 @@ public class StoreTest {
 
     @Test
     void getTotalCost() {
+
         Store store = new Store();
+        store.addProduct(new Product("A", 1.25));
+        store.addProduct(new Product("B", 4.25));
+        store.addProduct(new Product("C", 1));
+        store.addProduct(new Product("D", 0.75));
 
         assertEquals(13.25, store.calculateTotalCost("ABCDABA"));
     }
